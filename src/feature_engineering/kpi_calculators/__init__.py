376e4b9978d977ddcc4f21f98338b11c46a50d8c
krookskala <ismailsariarslan7@gmail.com>
2026-04-08 01:01:35 +0200

    Add KPI Calculators Package, defining the module structure and exposing various KPI calculation components for enhanced football performance analysis. This includes defensive, passing, shooting, creation, spatial, and dribbling KPIs.

---
+++ src/feature_engineering/kpi_calculators/__init__.py
@@ -0,0 +1,12 @@
+"""
+KPI Calculators Package
+"""
+
+__all__ = [
+    'defensive_kpis',
+    'passing_kpis',
+    'shooting_kpis',
+    'creation_kpis',
+    'spatial_kpis',
+    'dribbling_kpis'
+]
